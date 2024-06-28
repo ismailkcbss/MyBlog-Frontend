4f92ef2da95309a04222fb2c0535d0485485b644
ismailkcbss <ismailcankocabas@gmail.com>
2024-06-28 22:33:46 +0300

    rep protectedroute - general page

--- src/protected.route.js
+++ src/protected.route.js
@@ -1,10 +1,9 @@
 import { Route, Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import * as storage from './storage.helper';
-import jwtDecode from 'jwt-decode';
 import { useSelector } from 'react-redux';
 
 
-
+// Giriş yapmamış kullanıcıların tespiti için : Orneğin admin kontrolleri için olabilir.
 const ProtectedPageRoute = ({ component: Component, ...rest }) => {
     const user = storage.GetCookie('userdt');
     return (
@@ -18,8 +17,10 @@ const ProtectedPageRoute = ({ component: Component, ...rest }) => {
     )
 }
 
+
+// Giriş Yapılı kullanıcı tespiti için : Orneğin login sayfasına dönmemeli
 const ProtectedReturnPage = ({ component: Component, ...rest }) => {
-    const userData = useSelector((state) => state.user)
+    const userData = useSelector((state) => state.user);
     return (
         <Route
             {...rest}
@@ -32,20 +33,5 @@ const ProtectedReturnPage = ({ component: Component, ...rest }) => {
     )
 }
 
-const ProtectedSellerPage = ({ component: Component, ...rest }) => {
-    const sellerData = storage.GetCookie("userdt")
-    let data = jwtDecode(sellerData);
-    let seller = data.seller;
-    return (
-        <Route {...rest}
-            render={(props) => seller ? (
-                <Component />
-            ) : (
-                <Redirect to="/" />
-            )
-            }
-        />
-    )
-}
 
-export { ProtectedPageRoute, ProtectedReturnPage, ProtectedSellerPage }
+export { ProtectedPageRoute, ProtectedReturnPage }
